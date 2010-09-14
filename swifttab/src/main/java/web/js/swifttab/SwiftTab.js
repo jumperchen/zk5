59b42504cb93499505c37a0cb52574381bd2f671
tonylovejava@gmail.com <tonylovejava@gmail.com@2195c808-afca-11de-b8dd-ab4b6ce6d521>
2010-09-14 11:53:52 +0000

    change z-class to z-swifttab , avoid side effect for z-tab

--- swifttab/src/main/java/web/js/swifttab/SwiftTab.js
+++ swifttab/src/main/java/web/js/swifttab/SwiftTab.js
@@ -1,5 +1,13 @@
 zk.$package("swifttab");
 swifttab.SwiftTab = zk.$extends(zul.tab.Tab, {
 
-
+	bind_: function(desktop, skipper, after){
+		this.$supers('bind_', arguments);
+		this.$n("sort");
+	},
+	unbind_: function(){
+	},
+	getZclass:function(){
+		return (this._zclass !=null ? this._zclass : "z-swifttab");
+	}
 });
