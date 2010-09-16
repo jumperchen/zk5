ffc63c1ca193aa3955123ec78eff88ede9a2659a
simon.pai@gmail.com <simon.pai@gmail.com@2195c808-afca-11de-b8dd-ab4b6ce6d521>
2010-09-16 10:37:08 +0000

    init commit, revised structure: remove trunk, no webapp

---
+++ trunk/hatab/src/main/resources/web/js/hatab/Hatab.js
@@ -0,0 +1,25 @@
+/* Hatab.js
+
+	Purpose:
+		Horizontal Accordion Tabbox
+		
+	Description:
+		
+	History:
+		Sep 16, 2010 11:35:04 AM , Created by simon
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+*/
+/**
+ * A widget as horizontal accordion tabbox. Works like Tabbox in accordion mold,
+ * but in horizontal direction. 
+ * 
+ * <p>Default {@link #getZclass}: z-hatab.
+ */
+hatab.Hatab = zk.$extends(zul.Widget, {
+	
+	$define: {
+		
+	}
+	
+});
