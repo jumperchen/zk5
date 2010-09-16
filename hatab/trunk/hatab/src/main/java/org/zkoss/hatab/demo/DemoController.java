6fd90454eb15bc05827fdb0c860d3d78f777b690
simon.pai@gmail.com <simon.pai@gmail.com@2195c808-afca-11de-b8dd-ab4b6ce6d521>
2010-09-16 03:25:26 +0000

    mavan/zk project skeleton

---
+++ hatab/trunk/hatab/src/main/java/org/zkoss/hatab/demo/DemoController.java
@@ -0,0 +1,35 @@
+/* DemoController.java
+
+{{IS_NOTE
+	Purpose:
+		
+	Description:
+		
+	History:
+		Sep 16, 2010 9:19:40 AM , Created by simon
+}}IS_NOTE
+
+Copyright (C) 2010 Potix Corporation. All Rights Reserved.
+
+{{IS_RIGHT
+}}IS_RIGHT
+*/
+package org.zkoss.hatab.demo;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+/**
+ * 
+ * @author simon
+ */
+public class DemoController extends GenericForwardComposer {
+	
+	//Window win;
+	
+	public void doAfterCompose(Component comp) throws Exception {
+		super.doAfterCompose(comp);
+		
+	}
+	
+}
