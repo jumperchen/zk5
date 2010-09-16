c7684c8238314ac9c846a73be9898b43f3152253
tonylovejava@gmail.com <tonylovejava@gmail.com@2195c808-afca-11de-b8dd-ab4b6ce6d521>
2010-09-16 02:17:25 +0000

    fix the bug in javascript error.

--- trunk/swifttab/src/main/java/web/js/swifttab/Swifttab.js
+++ trunk/swifttab/src/main/java/web/js/swifttab/Swifttab.js
@@ -1,94 +1,80 @@
 (function(){
-    //private  front_
-    // protected end_
-    function _getNextX(helper){
-        if (helper.next("." + zcls).size() != 0) {
-            return helper.next().offset().left;
+
+    function _getNextX(helper, filter){
+        if (helper.next(filter).size() != 0) {
+            return helper.next().offset().left - helper.offset().left;
         }
         return -1;
     }
-    function _getPrevX(helper){
-        if (helper.prev("." + zcls).size() != 0) {
-            return helper.prev().offset().right;
+    function _getPrevX(helper, filter){
+        if (helper.prev(filter).size() != 0) {
+            return 0;
         }
-        return -1;
+        return 1;
     }
 
-    //TODO remove me
-    function _inspectAll(){
-        var datas = [];
-        jq(".z-swifttab").each(function(){
-            datas.push(jq(this).offset().left);
-        });
-
-        return (datas.join(";"));
-    }
     swifttab.Swifttab = zk.$extends(zul.tab.Tab, {
-        $define: {},
-        $init: function(){
-            //onChange
-            //onSelect
-            //onClose
+		_updateBound:function(ghost){
+
+		},
+        _makeSortable: function(){
+            var handle = this.$n("sort"), instance = jq(this.$n()), zcls = this.getZclass(), nextX = 0, prevX = 0 , lastBound;
+            if (handle && !this._drag) {
+                handle.style.cursor = "move";
+                this._drag = new zk.Draggable(this, null, {
+                    handle: handle,
+                    fireOnMove: false,
+                    constrint: "horizontal",
+                    starteffect: function(){
+
+                    },
+                    ghosting: function(dg, ofs, evt){
+                        var ghost = instance.clone();
+                        ghost.css("position", "relative").css("opacity", "0.8");
+                        instance.after(ghost);
+                        instance.hide();
+                        nextX = _getNextX(ghost, "." + zcls);
+                        prevX = _getPrevX(ghost, "." + zcls);
+                        return ghost[0];
+                    },
+                    draw: function(dg, ofs, evt){
+                        var newX = ofs[0], jqNode = jq(dg.node);
+                        jqNode.css("left", newX);
+                        if (nextX != -1 && newX > nextX) {
+                            jqNode.next().after(jqNode);
+                            jqNode.css("left", "0");
+                            nextX = _getNextX(jqNode, "." + zcls);
+                            prevX = _getPrevX(jqNode, "." + zcls);
+                        } else if (prevX <= 0 && newX < prevX) {
+                            jqNode.prev().prev().before(jqNode);
+							//one more prve because the instance is prev
+                            jqNode.css("left", "0");
+                            nextX = _getNextX(jqNode, "." + zcls);
+                            prevX = _getPrevX(jqNode, "." + zcls);
+                        }
+                    },
+                    endghosting: function(dg, origin){
+                        var el = dg.node; //ghost
+                        jq(el).after(instance);
+                        instance.show();
+                        jq(el).remove();
+                    },
+                    endeffect: function(){
+
+                    },
+                    zIndex: 99999 //Bug 2929590
+                });
+            }
         },
         bind_: function(desktop, skipper, after){
             this.$supers(swifttab.Swifttab, 'bind_', arguments);
-            var handler = jq(this.$n("sort")), instance = jq(this.$n()), zcls = this.getZclass();
-
-            handler.css("cursor", "move");
-            handler.mousedown(function(){
-                console.log("down");
-                var helper = instance.clone(), nextX = 0, prevX = 0, currentX = 0;
-                instance.after(helper);
-                instance.hide();
-                jq(window.body).after(instance);
-
-                nextX = getNextX(helper);
-                console.log(nextX);
-                prevX = getPrevX(helper);
-                currentX = helper.offset().left;
-
-                helper.css("position", "relative").css("opacity", "0.2");
-                jq(window).bind("mousemove.tab_drag", {
-                    instance: instance,
-                    helper: helper
-                }, function(e){
-                    //TODO check this
-                    var newX = e.pageX - currentX;
-                    helper.css("left", newX);
-					console.log(newX);
-                    if (nextX != -1 && newX > nextX) {
-                        helper.next().after(helper);
-                        helper.css("left", "0");
-                        currentX = helper.offset().left;
-                        nextX = getNextX(helper);
-                        prevX = getPrevX(helper);
-                        console.log(currentX + ":"+ e.pageX +":" +nextX+" , "+inspectAll());
-                    } else if (prevX != -1 && newX < prevX) {
-                        helper.prev().before(helper);
-						helper.css("left", "0");
-                        currentX = helper.offset().left;
-                        nextX = getNextX(helper);
-                        prevX = getPrevX(helper);
-                    }
-                });
-                jq(window).mouseup(function(){
-                    jq(window).unbind("mousemove.tab_drag");
-                    console.log("up");
-                    helper.after(instance);
-                    instance.show();
-                    helper.remove();
-                });
-                return false;
-            });
-
-
+            this._makeSortable();
         },
         unbind_: function(){
-            //destroy
-            this.$supers(swifttab.SwiftTab, "unbind_", arguments);
+            this.$supers(swifttab.Swifttab, "unbind_", arguments);
         },
         getZclass: function(){
-            return (this._zclass != null) ? this._zclass : "z-swifttab";
+            return (this._zclass != null ? this._zclass : "z-swifttab");
         }
     });
 })();
