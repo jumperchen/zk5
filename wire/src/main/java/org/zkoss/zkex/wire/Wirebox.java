ccda842c88a4ece1cab637a2ffa8730bbd9ad6a9
tonylovejava@gmail.com <tonylovejava@gmail.com@2195c808-afca-11de-b8dd-ab4b6ce6d521>
2010-10-11 05:06:06 +0000

    implements Wirebox's method.

--- wire/src/main/java/org/zkoss/zkex/wire/Wirebox.java
+++ wire/src/main/java/org/zkoss/zkex/wire/Wirebox.java
@@ -1,18 +1,57 @@
 package org.zkoss.zkex.wire;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.zkoss.zk.ui.sys.ContentRenderer;
 import org.zkoss.zul.impl.XulElement;
 
+public class Wirebox extends XulElement implements Serializable {
 
+	private ArrayList<Wire> _wires;
 
-public class Wirebox extends XulElement {
-	protected void renderProperties(ContentRenderer renderer) throws IOException {
-		super.renderProperties(renderer);
+	private String _points = "";
+
+	public Wirebox() {
+		_wires = new ArrayList<Wire>();
 	}
 
-	public String getZclass(){
-		return this._zclass ==null ? "z-wirebox" : this._zclass;
+	private void addWire(Wire w) {
+
+		_wires.add(w);
+
+	}
+
+	private void removeWire(Wire w) {
+		_wires.remove(w);
+	}
+
+	public List<Wire> getWires() {
+		// TODO check here if risk to let user get real instance for list
+		// i mean , user can really modifly the collection . by Tony
+		return _wires;
+	}
+
+	private void setPoints(String points, boolean ignoreUpdate) {
+		_points = points;
+		if (!ignoreUpdate) {
+			smartUpdate("points", _points);
+		}
+	}
+
+	public void setPoints(String points) {
+		setPoints(points, false);
+	}
+
+	protected void renderProperties(ContentRenderer renderer) throws IOException {
+		super.renderProperties(renderer);
+
+		render(renderer,"points",_points);
+	}
+
+	public String getZclass() {
+		return this._zclass == null ? "z-wirebox" : this._zclass;
 	}
 }
