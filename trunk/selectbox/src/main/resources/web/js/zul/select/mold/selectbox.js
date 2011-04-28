232ee8228a306ccf8cb6a70e594ecf1b58e8f161
tonylovejava@gmail.com <tonylovejava@gmail.com@2195c808-afca-11de-b8dd-ab4b6ce6d521>
2011-04-28 03:41:12 +0000

    Fix for Issue 1

--- trunk/selectbox/src/main/resources/web/js/zul/select/mold/selectbox.js
+++ trunk/selectbox/src/main/resources/web/js/zul/select/mold/selectbox.js
@@ -1,6 +1,6 @@
 function (out) {
 	out.push('<select', this.domAttrs_(), '>');
-	var s = $eval(this.items);
+	var s = $eval(this.items) || [] ;
 	for (var i = 0, j = s.length; i < j; i++) {
 		out.push('<option');
 		if (this._selectedIndex > -1 && this._selectedIndex == i)
