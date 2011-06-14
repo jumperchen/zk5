1e7f743bd9d185b270b61d02eaec9327ffdde996
simonpai <simonpai@2195c808-afca-11de-b8dd-ab4b6ce6d521>
2011-06-14 10:48:10 +0000

    fine tune

--- trunk/canvas4z/src/main/resources/web/js/canvas/Shape.js
+++ trunk/canvas4z/src/main/resources/web/js/canvas/Shape.js
@@ -20,8 +20,6 @@ canvas.Shape = zk.$extends(canvas.Drawable, {
 		this.$super('$init');
 	}
 	
-	
-	
 });
 
 /**
@@ -29,7 +27,7 @@ canvas.Shape = zk.$extends(canvas.Drawable, {
  */
 canvas.Rectangle = zk.$extends(canvas.Shape, {
 	
-	$init: function(x,y,w,h){
+	$init: function (x, y, w, h) {
 		this.$super('$init');
 		this.objtp = "rect";
 		this.obj = new zk.Object();
@@ -38,50 +36,49 @@ canvas.Rectangle = zk.$extends(canvas.Shape, {
 		this.obj.w = w;
 		this.obj.h = h;
 	},
-	
 	/**
 	 * 
 	 */
-	setPos: function(x,y){
+	setPos: function (x, y) {
 		this.obj.x = x;
 		this.obj.y = y;
 		return this;
 	},
-	
 	/**
 	 * 
 	 */
-	setSize: function(w,h){
+	setSize: function (w, h) {
 		this.obj.w = w;
 		this.obj.h = h;
 		return this;
 	},
-	
 	/**
 	 * Scales the rectangle. Reference point is (0,0)
 	 */
-	scale: function(a,b){
+	scale: function (a, b) {
 		this.obj.x *= a;
 		this.obj.y *= b;
 		this.obj.w *= a;
 		this.obj.h *= b;
 	},
-	
 	/**
 	 * Translate the rectangle
 	 */
-	translate: function(dx, dy){
+	translate: function (dx, dy) {
 		this.obj.x += dx;
 		this.obj.y += dy;
 	},
-	
 	/**
 	 * Return true if (x, y) is strictly inside the boundary of the rectangle.
 	 */
-	contains: function(x, y){
-		if(!x || !y) return false;
-		var sx = this.obj.x, sy = this.obj.y;
-		return sx < x && x < sx + this.obj.w && sy < y && y < sy + this.obj.h;
+	contains: function (x, y) {
+		if(!x || !y) return false; // TODO: check
+		var sx = this.obj.x, 
+			sy = this.obj.y;
+		return sx < x 
+			&& x < sx + this.obj.w 
+			&& sy < y 
+			&& y < sy + this.obj.h;
 	}
 	
 });
@@ -91,82 +88,74 @@ canvas.Rectangle = zk.$extends(canvas.Shape, {
  */
 canvas.Path = zk.$extends(canvas.Shape, {
 	
-	$init: function(){
+	$init: function () {
 		this.$super('$init');
 		this.objtp = "path";
 		this.obj = new zk.Object();
 		this.obj.sg = [];
 	},
-	
 	/**
 	 * 
 	 */
-	beginPath: function(){
+	beginPath: function () {
 		this.obj.sg = [];
 		return this;
 	},
-	
 	/**
 	 * 
 	 */
-	closePath: function(){
+	closePath: function () {
 		var s = new Object();
 		s.tp = "cl";
 		s.dt = [];
 		this.obj.sg.push(s);
 		return this;
 	},
-	
 	/**
 	 * 
 	 */
-	moveTo: function(x, y){
+	moveTo: function (x, y) {
 		var s = Object();
 		s.tp = "mv";
 		s.dt = [x,y];
 		this.obj.sg.push(s);
 		return this;
 	},
-	
 	/**
 	 * 
 	 */
-	lineTo: function(x, y){
+	lineTo: function (x, y) {
 		var s = Object();
 		s.tp = "ln";
 		s.dt = [x,y];
 		this.obj.sg.push(s);
 		return this;
 	},
-	
 	/**
 	 * 
 	 */
-	quadTo: function(cpx, cpy, x, y){
+	quadTo: function (cpx, cpy, x, y) {
 		var s = Object();
 		s.tp = "qd";
 		s.dt = [cpx, cpy, x,y];
 		this.obj.sg.push(s);
 		return this;
 	},
-	
 	/**
 	 * 
 	 */
-	bezierTo: function(cpx1, cpy1, cpx2, cpy2, x, y){
+	bezierTo: function (cpx1, cpy1, cpx2, cpy2, x, y) {
 		var s = Object();
 		s.tp = "bz";
 		s.dt = [cpx1, cpy1, cpx2, cpy2, x, y];
 		this.obj.sg.push(s);
 		return this;
 	},
-	
 	// arcTo
-	
 	/**
 	 * Transforms the path
 	 */
-	transform: function(m){
+	transform: function (m) {
 		var sg = this.obj.sg;
 		for(var i=sg.length;i--;){
 			var dt = sg[i].dt;
@@ -177,41 +166,34 @@ canvas.Path = zk.$extends(canvas.Shape, {
 			}
 		}
 	},
-	
 	/**
 	 * Scales the path. Reference point is (0,0).
 	 */
-	scale: function(a,b){
+	scale: function (a,b) {
 		this.transform([a,0,0,b,0,0]);
 	},
-	
 	/**
 	 * Translates the path
 	 */
-	translate: function(dx, dy){
+	translate: function (dx, dy) {
 		this.transform([1,0,0,1,dx,dy]);
 	},
-	
 	/**
 	 * 
 	 */
-	clone: function(){
+	clone: function () {
 		var p2 = new canvas.Path();
 		p2._copyObj(this);
 		p2._copyState(this);
 	},
-	
-	/**
-	 * Return true if (x, y) is strictly inside the boundary of the path
-	 */
-	contains: function(x, y){
-		if(!x || !y) return false;
+	//@Override
+	contains: function (x, y) {
+		//if(!x || !y) return false;
 		// TODO: port Java 2D's algorithm to here
 		return false;
 	},
-	
 	// copy object data from path
-	_copyObj: function(path){
+	_copyObj: function (path) {
 		this.obj.sg = [];
 		var sg1 = path.obj.sg;
 		for(var i=sg1.length;i--;){
@@ -223,4 +205,5 @@ canvas.Path = zk.$extends(canvas.Shape, {
 			}
 		}
 	}
+	
 });
